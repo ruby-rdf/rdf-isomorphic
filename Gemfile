2296d9b85edbcbbd8028b993621adbbe2ae5cf5a
Gregg Kellogg <gregg@greggkellogg.net>
2019-01-01 13:28:16 -0800

    Add simplecov, and remove mri_19 support.

--- Gemfile
+++ Gemfile
@@ -6,5 +6,10 @@ gem "rdf",      git: "git://github.com/ruby-rdf/rdf.git", branch: "develop"
 gem "rdf-spec", git: "git://github.com/ruby-rdf/rdf-spec.git", branch: "develop"
 
 group :debug do
-  gem "debugger", platforms: [:mri_19]
+  gem "byebug", platforms: :mri
+end
+
+group :test do
+  gem 'simplecov',  require: false, platform: :mri
+  gem 'coveralls',  require: false, platform: :mri
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,17 @@ $:.unshift(File.expand_path("../../lib/", __FILE__))
 
 require "bundler/setup"
 require 'rdf'
+
+begin
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ])
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+rescue LoadError => e
+  STDERR.puts "Coverage Skipped: #{e.message}"
+end
